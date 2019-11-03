64ee8530918f38775cbc405982db8d660297df6b
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-11-03 23:42:05 +0100

    Update buildpack-run.sh to support plugins (#9)

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+mv plugins/ ../plugins/ # Save the plugins folder, below methode doesn't cover folders.
 rm -rf $(find * -name "*" ! -name "Watchdog.jar" ! -name "Procfile" ! -name "plugins/*") # Remove everything except Procfile, Watchdog.jar and plugins folder
+mv ../plugins/ . # Save the plugins folder placing it back
 java -jar Watchdog.jar # Create needed config files by running Watchdog.jar
